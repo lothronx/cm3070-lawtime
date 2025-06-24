09d0aff6544f29e2e4a086d074c32bf8835d648c
WU Yue <yuewuxd@gmail.com>
2025-06-24 12:39:10 +0800

    -

--- mobile-app/utils/supabase.ts
+++ mobile-app/utils/supabase.ts
@@ -2,17 +2,13 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 // eslint-disable-next-line import/no-unresolved
-import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY } from "@env";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
-export const supabase = createClient(
-  EXPO_PUBLIC_SUPABASE_URL || "",
-  EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  }
-);
+export const supabase = createClient(SUPABASE_URL || "", SUPABASE_ANON_KEY || "", {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
